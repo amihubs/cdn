2a520e291dff4336575eebe3b4c7e27a456651eb
Amihub <amihub@mail.com>
2017-11-29 10:41:50 +0700

    xbxcb

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-      $('body').append('<div id="toTop" class="btn btn-info"><i class="fa fa-angle-up" aria-hidden="true"></i></div>');
+      $('body').append('<div id="bTt" class="btn btn-info"><i class="fa fa-angle-up" aria-hidden="true"></i></div>');
     	$(window).scroll(function () {
 			if ($(this).scrollTop() != 0) {
-				$('#toTop').fadeIn();
+				$('#bTt').fadeIn();
 			} else {
-				$('#toTop').fadeOut();
+				$('#bTt').fadeOut();
 			}
 		}); 
-    $('#toTop').click(function(){
+    $('#bTt').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
