396bcb32a3fab221af3e4b7947379df85eb78c28
Amihub <amihub@mail.com>
2017-12-02 09:28:41 +0700

    tgfd

--- js/app.js
+++ js/app.js
@@ -12,3 +12,11 @@ $(document).ready(function(){
         return false;
     });
 });
+(function(w, d){
+	var b = d.getElementsByTagName('body')[0];
+	var s = d.createElement("script"); s.async = true;
+	var v = !("IntersectionObserver" in w) ? "8.5.2" : "10.3.5";
+	s.src = "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/" + v + "/lazyload.min.js";
+	w.lazyLoadOptions = {};
+	b.appendChild(s);
+}(window, document));
